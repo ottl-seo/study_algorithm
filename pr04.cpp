c2678c8896784ce8a82e5c6fc5c1117470a69ffe
Yoonseo KIM <61778930+yoonseo-hi@users.noreply.github.com>
2020-09-19 01:10:37 +0900

    day2
    20.09.18

---
+++ pr04.cpp
@@ -0,0 +1,30 @@
+/* 4. 마름모 출력하기 */
+
+#include <stdio.h>
+int main() {
+	int n;
+	printf("Input n: "); //라인 수로 입력 받기
+	scanf_s("%d", &n);
+
+	// 위 삼각형
+	for (int i = 0; i < n; i++) { // n줄
+		for (int b = 0; b < (n - i); b++) //blank
+			putchar(' ');
+		for (int s = 0; s < (2 * i - 1); s++) //star
+			putchar('*');
+		putchar('\n');
+	}
+	// 아래 삼각형
+	for (int i = n; i > 0; i--) {
+		for (int b = 0; b < (n - i); b++) //blank
+			putchar(' ');
+		for (int s = 0; s < (2 * i - 1); s++) //star
+			putchar('*');
+		putchar('\n');
+	}
+	return 0;
+}
+
+// 함수화는 하지 않음
+// 줄 수로 따지는 게 더 편하다
+// blank와 star는 전체를 한 번에 제어 X -- 한줄에 찍히는 걸 반복문화 하는 거임
