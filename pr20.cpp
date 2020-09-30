93082556dd3531fdc3df354423e21adf2bbed7bd
Yoonseo KIM <61778930+yoonseo-hi@users.noreply.github.com>
2020-09-30 17:40:16 +0900

    day14
    나가기전에 후다닥
    동적할당이랑 문자열 입력받는걸로 응용해보려 했는데 그건 못했다.
    결국 책 풀이가 정답인건가,,, ,,,
    그리고 포인터랑 배열의 차이가 뭐지... 왜 문자열 전달할 때 포인터로 하면 실행이 안되고 배열[100]으로 하면 되는거지!!!!!!
    알수 없다 ㅠ.ㅠ
    부트스트랩도 적용못했다 ㅠ.ㅠ 아니 부트 강의도 못들었다.
    내일부터 하면되니까! !!! 추석은 기니까 할 수 있다. 그럼 이제 빨리 준비하고 나가야지

---
+++ pr20.cpp
@@ -0,0 +1,35 @@
+/* 책 예제 20. 공백 문자 제거(left)
+	(isspace(*str)==1)이면, ...~  */
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h> //isspace() 함수
+// #include <stdlib.h> --malloc 어떻게 쓰는거지....
+
+char* remove_sp(char* strr);
+
+int main(){
+	//char* strr = (char*)malloc(sizeof(char)); //문자열
+	char strr[100];
+	char* tlqkf;
+	/*
+	printf("Input String: ");
+	scanf_s("%s", strr); //입력받기 %s, 포인터 참조는 없다
+	*/
+	strcpy_s(strr, "\n\t  hi");
+	printf("<< Before >>%s\n", strr);
+	tlqkf = remove_sp(strr);
+	printf("<< After >>\n%s", tlqkf);
+
+	return 0;
+}
+char* remove_sp(char* strr) {
+	int i = 0;
+	char ch = *(strr + i);
+
+	while (*strr) {
+		if (!isspace(*strr) == 1) { return strr; } //블랭크 아니면 그대로 리턴
+		else
+			*strr++;
+	}
+	return (char*)""; //빈 문자열 리턴
+}
