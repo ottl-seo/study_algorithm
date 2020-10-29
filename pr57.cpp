5036ca2e120ac27b361aa4197eb9cb2e49368877
Yoonseo KIM <61778930+ottl-seo@users.noreply.github.com>
2020-10-29 20:16:19 +0900

    day43
    20.10.29 현암기 강의 들어야되는데 너무 듣기 싫어서 일기쓰는 중. 그래도 걱정한 것보단 과제 무난한듯!! 아닌가
    너무 부담 갖지말고 조금 마음 편히 지내는 게 좋은 것 같다 내가 동동거리고 스트레스 받는다고 해서 결과가 더 잘 나오는 것도 아니니... 좀더 단단한 사람이 되어야지! 그리고 사소한 것을 칭찬하고 행복을 느끼는 사람이 될 거다. 매일 다짐하는 거지만 실제로 지키긴 참 어려운 것 같다. ㅎㅎ 내일은 현수님 만날거고, 그 다음날은 내 생일이다! 악성에 현암기에 할 게 많아서 며칠간 매우 답답해했지만, 좀 마음을 편하게 가지면 좋겠다. 헤헤 시험 끝나면 운동도 다시 가고 몸도 마음도 더 건강하게 지내봐야징
    일기 쓰고 싶어서 들어왔는데 오늘 계속 집에 누워만 있어서인지, 쓸말은 별로 없다. 집에서 맛있는 밥 먹고, 늦잠자고 간식먹고 하다가 너무 쳐지는 것 같아서 회의할 겸 투썸에 왔다. 핑크색 폴로 모자를 썼당 크크 그리고 커피랑 딸기케이크를 먹었다. 9시에 회의해야한다. 그냥 빨리 이번 학기가 지나갔으면 좋겠다. 올해가 지나가고 후련하게 겨울쯤에 2021년 계획을 쭉 짜보고 싶다. 실전창업교육은 내년에도 다시 한번 듣고싶다! 근영이 꼬셔봐야징 음 주저리주저리 많이 쓴 것 같다. 이제 강ㅇ의 듣자...... ^__^

--- pr57.cpp
+++ pr57.cpp
@@ -1,6 +1,8 @@
-/* 이진트리 traversal */
+/* 3- 레벨 순회 코드 -- pass 
+* 4. 수식 트리 계산 */
 #include <stdio.h>
 #include <stdlib.h>
+#define MAX 100
 
 typedef struct TreeNode {
 	int data;
@@ -8,46 +10,35 @@ typedef struct TreeNode {
 }TreeNode;
 
 TreeNode n1 = { 1,NULL,NULL };
-TreeNode n2 = { 2,&n1,NULL };
-TreeNode n3 = { 3,NULL,NULL };
-TreeNode n4 = { 4,NULL,NULL };
-TreeNode n5 = { 5,&n3,&n4 };
-TreeNode n6 = { 6,&n2,&n5 };
-TreeNode *root = &n6;
+TreeNode n2 = { 4,NULL,NULL };
+TreeNode n3 = { '*',&n1,&n2 };
+TreeNode n4 = { 16,NULL,NULL };
+TreeNode n5 = { 25,NULL,NULL };
+TreeNode n6 = { '+',&n4,&n5 };
+TreeNode n7 = { '+',&n3,&n6 };
+TreeNode* exp = &n7;
 
-void inorder(TreeNode* root) { /* 중위 순회 (LVR) */
-	if (root) {
-		inorder(root->left);
-		printf("[%d]", root->data);
-		inorder(root->right);
+int evaluate(TreeNode* exp) {
+	if (exp == NULL) { return 0; } //노드가 없는 경우
+	else if (exp->left == NULL && exp->right == NULL) { return exp->data; } //말단 노드인 경우
+	else {
+		int op1 = evaluate(exp->left);
+		int op2 = evaluate(exp->right);
+		printf("%2d %c %2d\n", op1, exp->data, op2);
+		switch (exp->data)
+		{
+		case '+':return op1 + op2; break;
+		case '-':return op1 - op2; break;
+		case '*':return op1 * op2; break;
+		case '/':return op1 / op2; break;
+		}
 	}
-}
-void preorder(TreeNode* root) { /* 전위 순회 (VLR) */
-	if (root) {
-		printf("[%d]", root->data);
-		preorder(root->left);
-		preorder(root->right);
-	}
-}
-void postorder(TreeNode* root) { /* 후위 순회 (LRV) */
-	if (root) {
-		postorder(root->left);
-		postorder(root->right);
-		printf("[%d]", root->data);
-	}
+	return 0;
 }
 int main() {
-	printf("<Inorder>\n");
-	inorder(root);
-	printf("\n");
-
-	printf("<Preorder>\n");
-	preorder(root);
-	printf("\n");
-
-	printf("<Postorder>\n");
-	postorder(root);
-	printf("\n");
+	
+	printf("1*4 + 16+25\n===================\n");
+	printf("===================\nResult= %d\n", evaluate(exp));
 
 	return 0;
 }
