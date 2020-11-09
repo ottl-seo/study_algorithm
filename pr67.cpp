d88d06e85cb46f69046920d0dbc0bfcd4a18621e
Yoonseo KIM <61778930+ottl-seo@users.noreply.github.com>
2020-11-09 22:40:23 +0900

    day52
    집부 엠티중 쉬는시간
    술먹다가 유림이랑 이화이언 피드백 안한거 생각나서 뛰쳐나가서 씀 30분 걸림 ㅡㅡ

--- pr67.cpp
+++ pr67.cpp
@@ -30,19 +30,19 @@ void insert_max_heap(HeapType* h, element item) { //max heap에 노드 추가
 }
 element delete_max_heap(HeapType* h){
 	int parent = 1, child = 2; // 두 번째 노드부터 root 노드와 비교 시작
-	element item = h->heap[1]; // 삭제되는 노드(삭제 후 반환)
-	element temp = h->heap[h->heap_size--]; // 말단 노드를 temp에 대입(노드 삭제 후 heap_size가 감소)
+	element item = h->heap[1]; // root노드를 delete, 이후 반환할 용도.
+	element temp = h->heap[h->heap_size--]; // 말단 노드를 temp에 대입(노드 삭제 후 heap_size가 감소!! )
 	
-	while (child <= h->heap_size) { // 최대 말단까지 트리를 내려가면서 자식노드 key값과 temp key 비교
-		if ((child < h->heap_size) && (h->heap[child].key < h->heap[child + 1].key)) // sibling이 있을 때 둘 중 큰 값 찾기
-			child++; // sibling이 비교대상 노드가 됨(subtree 선택)
-		if (temp.key >= h->heap[child].key) break; // 자식 노드보다 신규 노드 key가 크면 위치 확정되어 반복 종료
+	while (child <= h->heap_size) { // 노드 끝까지 반복
+		if ((child < h->heap_size) && (h->heap[child].key < h->heap[child + 1].key)) // (1)sibling 처리
+			child++; // 더 클 경우 오른쪽으로 child 변경
+		if (temp.key >= h->heap[child].key) break; // (2)마지막 노드의 sibling 처리
 		
 		h->heap[parent] = h->heap[child]; // 자식 노드 key가 더 크면 parent 노드에 child 노드 복사
 		parent = child;
 		child *= 2;
 	}
-	h->heap[parent] = temp; // 확정위치(parent)에 신규노드 insert
+	h->heap[parent] = temp; // 마지막 child의 위치(parent)에 신규노드 insert
 	return item;
 
 }
