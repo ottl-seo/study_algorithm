3b37072e47f55d6a4392b2dc76fdde68b3c56ae0
Yoonseo KIM <61778930+ottl-seo@users.noreply.github.com>
2020-11-18 23:58:17 +0900

    day61
    기분 나아졌다 헤헤 아까 6시쯤이었나. 일기를 써도, 유튜브를 봐도, 책을 읽어도 기분이 나아지지 않았고 숨쉬긴 죽을만큼 힘들었고 잠도 안왔다. 그래서 대체 뭘해야 할지 모르겠는데 뭘 하지 않으면 우울해서 견디지 못할 것 같았다. 이거 쓰고 있으니까 또 답답해진다. 근데 그렇게 10분 정도 힘들어하고 있을때 재우님한테 카톡이 왔다. 뭐 그냥 안부묻는 내용이었는데 뭔가 기대하지 않았던 사람이라서 더 반가웠나. 혼자 끙끙 앓다가 뭔가 고마웠다. 그래서 그냥 얘기좀 하고, 다음달에 같이 전시회 가기로 했는데 그거에 대해서 생각하다보니 기분이 좋아졌다. 아까 언제 그렇게 힘들었냐는듯 금방 나아졌다. 중간만 해라 제발,,, 하하 현수님도 그렇고 의사쌤도, 간호사님도 술 커피 담배 절대 안된다고 했다. 애초에 호흡곤란도 신경증 증상이니까 카페인 알코올은 최대한 자제하는 게 맞는 거였다. 저번주에 술을 먹지 말았어야 했는데... 먹지말랬는데 그냥 먹고싶다고 먹고 다하면 결국 힘든 건 나라는걸 뼈저리게 느꼈다... 이제 남은 2020 동안이라도 안먹어야지. 내년초엔 술 마실 수 있기를. 오늘 데구 공부를 할까말까 일기썼으니까 2개만 할까..! ^^ 일단 끄읕

--- pr78.cpp
+++ pr78.cpp
@@ -1,1 +1,23 @@
 /**/
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 100
+#define TRUE 1
+#define FALSE 0
+#define INF 1000 // 무한대 표현
+
+#define SWAP(x,y,t)((t)=(x),(x)=(y),(y)=(t))
+
+int vertex[MAX]; //vertex 배열
+int parent[MAX]; //부모 vertex값 저장 배열
+
+struct Edge { //edge 구조
+	int start, end, weight;
+};
+typedef struct GraphType {
+	int n; //edge 개수
+	int vertexCnt; //vertex 개수
+	struct Edge edges[2 * MAX]; //edge 수는 vertex 수의 두 배 (MST이므로)
+}GraphType;
+
+/* ==================== Quick Sort ======================= */
