06fee724b105cb202cc3b75e69bc70acd59eef3b
Yoonseo KIM <61778930+ottl-seo@users.noreply.github.com>
2020-11-26 17:49:36 +0900

    day67
    11 26 목
    노래 들으면서 다익스트라 복습했다
    낮에 일어나서 피자 먹고 노래 듣고 침대에 누워있다가, 악성 강의나 들을까 하고 일어났다. 강의 듣기 싫어서 저번주 데구 복습차원으로 코딩부터 하나 했는데, 저번엔 계속 쳐다보고 있어도 이해 안되던게 오늘 머리가 맑을 때 다시 보니 깔끔하게 이해가 되고 재밌었다. 
    진짜 하기 싫은데 해야한다며 억지로 했던 게 많았는데, 오늘처럼 스스로 하고 싶어서(?) 하니까 기분이 상쾌하고 좋다. 
    앞으로도 난 아마 평생 코딩하는 일을 계속 할텐데, 하기싫을 때 죽상으로 하기보다는 오늘처럼 가볍게 꾸준히 하는 게 실력에도, 내 멘탈에도 훨씬 좋을 거라는 걸 느낀 것 같다. :) 뭐든 즐기는 사람을 이길 순 없다고, 나도 어차피 내가 좋아서 시작한 거, 무리해서 괜히 흥미를 떨어트리지 말고 적당히 재미도 느껴가면서 오래오래 하고 싶다.
    오늘은 간만에 무기력하지도 않고 잔잔히, 기분좋은 날이다 :D

---
+++ pr84.cpp
@@ -0,0 +1,87 @@
+/* day 67 - 다익스트라 오류 해결 및 복습 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define TRUE 1
+#define FALSE 0
+#define MAX_VERTICES 100
+#define INF 1000L //무한대(연결이 없는 경우)
+
+typedef struct GraphType {
+	int n; //정점 개수
+	int weight[MAX_VERTICES][MAX_VERTICES]; // 각 정점 사이 거리 표현
+}GraphType;
+
+int distance[MAX_VERTICES]; // start vertex로부터 각 정점까지의 최단 거리
+int found[MAX_VERTICES]; //방문한 정점 표시
+
+int choose(int distance[], int n, int found[]) { /* distance 중에서 가장 짧은 vertex번호를 리턴 */
+	int i, min, min_vertex;
+	min = INT_MAX;
+	min_vertex = -1; // 최소거리 갖는 vertex: 없는 vertex로 일단 초기화
+	for (i = 0; i < n; i++) {
+		if (distance[i] < min && !found[i]){
+			min = distance[i]; // min값 갱신
+			min_vertex = i; //최소거리 갖는 vertex
+		}
+	}
+	return min_vertex; //vertex 번호 반환
+}
+void print_status(GraphType* g) {
+	static int step = 1;
+	printf("STEP %d:\n", step++); //step 1, 2, ...
+	printf(" distance: ");
+	for (int i = 0; i < g->n; i++)
+		if (distance[i] == INF) { printf(" * "); }
+		else { printf("%2d ", distance[i]); }
+	printf("\n found: ");
+	for (int i = 0; i < g->n; i++)
+		printf("%2d ", found[i]);
+	printf("\n\n");
+}
+//
+void shortest_path(GraphType* g, int start) { // start_vertex 번호 받기
+	int i, u, w;
+
+	/* 1.초기화 */
+	for (int i = 0; i < g->n; i++){
+		found[i] = FALSE;
+		distance[i] = g->weight[start][i]; //start vertex로부터의 정점까지의 거리 (1,INF,3 등등...)
+	}
+	/* 2.start vertex 설정 */
+	found[start] = TRUE;
+	distance[start] = 0;
+
+	/* 3.최단 vertex를 찾아서 found에 추가 */
+	for (int i = 0; i < g->n - 1; i++) {
+		print_status(g); //step 출력
+		u = choose(distance, g->n, found); // 제일 작은 distance 갖는 vertex 반환 == u
+		found[u] = TRUE;
+
+		/* 4.u를 거쳐가는 게 더 짧을 경우, 갱신 */
+		for (w = 0; w < g->n; w++) {
+			if (!found[w]) { // !found 주의...
+				if (distance[u] + g->weight[u][w] < distance[w]) //더 짧을 경우
+					distance[w] = distance[u] + g->weight[u][w]; // distance[w]값 갱신
+			}
+		}
+	}
+}
+int main() {
+	GraphType g = {
+		7,
+		{
+		{0,7,INF,INF,3,10,INF},
+		{7,0,4,10,2,6,INF},
+		{INF,4,0,2,INF,INF,INF},
+		{INF,10,2,0,11,9,4},
+		{3,2,INF,11,0,INF,5},
+		{10,6,INF,9,INF,0,INF},
+		{INF,INF,INF,4,5,INF,0}
+		}
+	};
+	shortest_path(&g, 0); //0번 vertex에서 모든 vertex까지의 shortest path
+
+	return 0;
+}
